6e562d63cbcf10b3af170a91e07339c6a784c6d5
Adi-K-Coding <34950822+Adi-K-Coding@users.noreply.github.com>
2022-05-17 20:44:55 -0700

    olympicRings(IP)

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,13 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	Robot r1 = new Robot(200, 400);
+	Robot r2 = new Robot(500, 400);
+	Robot r3 = new Robot(800, 400);
+	Robot r4 = new Robot(400, 700);
+	Robot r5 = new Robot(400, 700);
 }
 
