3b63d9b58591d2a206ff4a62a2dd76f39d7385be
Adi-K-Coding <34950822+Adi-K-Coding@users.noreply.github.com>
2022-05-22 17:09:10 -0700

    olympic rings

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -3,11 +3,34 @@ package _01_Olympic_Rings;
 import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-	Robot r1 = new Robot(200, 400);
-	Robot r2 = new Robot(500, 400);
-	Robot r3 = new Robot(800, 400);
-	Robot r4 = new Robot(400, 700);
-	Robot r5 = new Robot(400, 700);
-}
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
 
+	public static void main(String[] args) {
+
+		Robot r1 = new Robot(200, 400);
+		Robot r2 = new Robot(600, 400);
+		Robot r3 = new Robot(1000, 400);
+		Robot r4 = new Robot(400, 600);
+		Robot r5 = new Robot(800, 600);
+
+		Robot[] rs = { r1, r2, r3, r4, r5 };
+
+		Thread[] robots = new Thread[5];
+
+		for (int i = 0; i < robots.length; i++) {
+			int x = i;
+			robots[x] = new Thread(() -> {
+				rs[x].penDown();
+				rs[x].turn(-90);
+				for (int t = 0; t < 360; t++) {
+					rs[x].move(3);
+					rs[x].turn(1);
+				}
+			});
+		}
+		for (int i = 0; i < robots.length; i++) {
+			robots[i].start();
+		}
+	}
+}
