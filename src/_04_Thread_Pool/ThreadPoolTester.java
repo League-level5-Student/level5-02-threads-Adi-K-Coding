7321e5ca60e27b857ab53fe6fa793768c0b7e278
Adi-K-Coding <34950822+Adi-K-Coding@users.noreply.github.com>
2022-07-10 16:32:39 -0700

    thread pool

---
+++ src/_04_Thread_Pool/ThreadPoolTester.java
@@ -0,0 +1,19 @@
+package _04_Thread_Pool;
+
+import _04_Thread_Pool.WorkQueue.Job;
+
+public class ThreadPoolTester {
+
+	public static void main(String[] args) {
+		WorkQueue wq = new WorkQueue();
+		for (int i = 0; i < 1000; i++) {
+			int x = i;
+			Job j = () -> {
+				System.out.println("Printing " + x + " from thread#" + Thread.currentThread().getId());
+			};
+			wq.addJob(j);
+		}
+		wq.shutdown();
+	}
+
+}
--- src/_04_Thread_Pool/WorkQueue.java
+++ src/_04_Thread_Pool/WorkQueue.java
@@ -1,8 +1,12 @@
 package _04_Thread_Pool;
 
+import java.lang.Thread.State;
+import java.util.ArrayDeque;
+
 public class WorkQueue implements Runnable {
+	private ArrayDeque<Job> jobQueue = new ArrayDeque<Job>();
 	private Thread[] threads;
-	private boolean isRunning = true;
+	private volatile boolean isRunning = true;
 
 	public WorkQueue() {
 		int totalThreads = Runtime.getRuntime().availableProcessors() - 1;
@@ -15,7 +19,15 @@ public class WorkQueue implements Runnable {
 
 	public void run() {
 		while (isRunning) {
-			System.out.println("Output from thread #" + Thread.currentThread().getId());
+			if (!performJob()) {
+				synchronized (jobQueue) {
+					try {
+						jobQueue.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 	}
 
@@ -24,6 +36,48 @@ public class WorkQueue implements Runnable {
 	}
 
 	public void shutdown() {
+		completeAllJobs();
 		isRunning = false;
+		synchronized (jobQueue) {
+			jobQueue.notifyAll();
+		}
+	}
+
+	public interface Job {
+		void perform();
+	}
+
+	public void addJob(Job j) {
+		synchronized (jobQueue) {
+			jobQueue.add(j);
+			jobQueue.notify();
+		}
+	}
+
+	public boolean performJob() {
+		Job j = null;
+		synchronized (jobQueue) {
+			if (!jobQueue.isEmpty()) {
+				j = jobQueue.remove();
+			}
+		}
+		if (j != null) {
+			j.perform();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void completeAllJobs() {
+		while (!jobQueue.isEmpty()) {
+			performJob();
+		}
+
+		for (int i = 0; i < threads.length; i++) {
+			if (threads[i].getState() != State.WAITING) {
+				i = -1;
+			}
+		}
 	}
 }
